d6eaf6e868c4ce1f996302688e2dc68f622d790c
fuzhongyi <fuzhongyi@doctorwork.com>
2020-03-24 18:22:21 +0800

    feat: command init

--- bin/dantd.js
+++ bin/dantd.js
@@ -6,22 +6,22 @@ const chalk = require("chalk");
 const ora = require("ora");
 
 program
-  .version("0.1.0")
-  .option("-i, init [name]", "初始化项目")
-  .parse(process.argv);
-
-if (program.init) {
-  const spinner = ora("正在下载").start();
-  download(
-    "gitlab:gitlab.aihaisi.com:/web/react-antd-template",
-    program.init,
-    function(err) {
-      spinner.stop();
-      if (!err) {
-        console.info(chalk.blueBright("下载成功"));
-      } else {
-        console.info(chalk.redBright("下载失败：" + err));
+  .command("init [name]")
+  .description("初始化项目")
+  .action(name => {
+    const spinner = ora("正在下载").start();
+    download(
+      "gitlab:git.doctorwork.com:fuzhongyi/react-antd-template",
+      name,
+      function(err) {
+        spinner.stop();
+        if (!err) {
+          console.info(chalk.greenBright("下载成功"));
+        } else {
+          console.info(chalk.redBright("下载失败：" + err));
+        }
       }
-    }
-  );
-}
+    );
+  });
+
+program.parse(process.argv);
